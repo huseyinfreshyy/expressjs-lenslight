ed668e06125e1696c9ee79a0c268ce80975907e3
Huseyin Taze <huseyintaze105@gmail.com>
2023-09-03 11:38:05 +0300

    get photo by id

--- routes/photoRouter.js
+++ routes/photoRouter.js
@@ -4,6 +4,7 @@ import * as photoController from '../controllers/photoController.js';
 const router = express.Router();
 
 router.route('/').post(photoController.createPhoto).get(photoController.getAllPhotos);
+router.route('/:id').get(photoController.getPhotoById)
 
 export default router;
 
